db3506fce9dfbe1ef96e940bcd33e802fd7aaa6d
Priscilla Mante Awuah <priscyawuah17@gmail.com>
2026-03-25 19:08:10 +0000

    added to pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,5 +1,9 @@
 import sys
+import pandas as pd
+
 print("arguments", sys.argv)
 
-day = int(sys.argv[1])
-print(f"Running pipeline for day {day}")
+month = int(sys.argv[1])
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+print(f"Running pipeline for month {month}")
