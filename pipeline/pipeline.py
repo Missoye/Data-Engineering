0e4f472c8e333714141bf8c2d62dc03d61af67bd
Priscilla Mante Awuah <priscyawuah17@gmail.com>
2026-03-30 17:05:27 +0000

    added to pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,5 +5,9 @@ print("arguments", sys.argv)
 
 month = int(sys.argv[1])
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df['month']=month
+
+df.to_parquet(f"output_month_{sys.argv[1]}.parquet")
 print(df.head())
+
 print(f"Running pipeline for month {month}")
